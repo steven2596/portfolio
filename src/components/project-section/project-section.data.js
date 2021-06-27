0f6b398505cc89cbf15634387399e275d2557d08
Aung Kaung Myat <aungkaungmyat2596@gmail.com>
2021-06-27 11:42:43 +0630

    images bug fixed

--- src/components/project-section/project-section.data.js
+++ src/components/project-section/project-section.data.js
@@ -1,14 +1,10 @@
-import img0 from '../../assets/images/t-metro.png';
-import img1 from '../../assets/images/wanderlust.png';
-import img2 from '../../assets/images/ticktask.png';
-
 const ProjectData = [
     {
         id: 0,
         title: 'T-Metro',
         description: 'T-Metro is an E-commerce website where people can buy albums and merch of the artist TWICE. Website features include product search, cart checkout, payment with Stripe, user registration and login.',
         tech: ['HTML', 'CSS', 'Sass', 'Javascript', 'React', 'Redux', 'Node.js', 'Firebase', 'Stripe API', 'Express'],
-        image: img0,
+        image: "https://i.ibb.co/0nhYVXP/t-metro.png",
         url: 'www.t-metro.com',
         repo: 'https://github.com/steven2596/t-metro'
     },
@@ -17,7 +13,7 @@ const ProjectData = [
         title: 'Wanderlust Travels',
         description: 'Wanderlust is the travel and tour agency which manages tour packages for Switzerland. It’s a landing page which showcases the agency’s services, tour packages it offers, benefits, etc.',
         tech: ['HTML', 'CSS', 'Sass', 'Javascript', 'React'],
-        image: img1,
+        image: "https://i.ibb.co/zHDCz0r/wanderlust.png",
         url: 'https://wanderlust-travels.netlify.app',
         repo: 'https://github.com/steven2596/wanderlust-travels'
     },
@@ -26,7 +22,7 @@ const ProjectData = [
         title: 'TickTask',
         description: 'TickTask is a full-stack web application which helps you to make a list of tasks with different priority levels and keep track of completed tasks. It also includes dark mode which helps reduce eye strain in low-light conditions.',
         tech: ['HTML', 'CSS', 'Sass', 'Javascript', 'React', 'Redux', 'Node.js', 'Express', 'MongoDB'],
-        image: img2,
+        image: "https://i.ibb.co/dMgN6C4/ticktask.png",
         url: 'www.ticktask.com',
         repo: 'https://github.com/steven2596/ticktask'
     }
